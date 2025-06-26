ee82ea03c0baa5c22a6d5f761b8f5f8e32c96e12
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-06-26 12:24:53 +0530

    Hello world

---
+++ dynamic_block/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "Roboshop" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = var.environment == "dev" ? "t3.micro" : "t3.small"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic and all outbound traffic"
+  dynamic "ingress" {
+    for_each = var.ingress_ports
+    content {
+      from_port        = ingress.value["from_port"]
+      to_port          = ingress.value["to_port"]
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow_all"
+  }
+}
